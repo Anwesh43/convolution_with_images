07b3e0320cbf5d1d913eb9604813113358c95c47
Anwesh43 <anweshthecool0@gmail.com>
2017-05-26 19:55:06 +0530

    using ImageConvolution for gaussian blur

--- gaussian_blur_convlution.py
+++ gaussian_blur_convlution.py
@@ -1,20 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from scipy.signal import convolve2d
-im = mpimg.imread('wenger.png')
-im = im.mean(axis=2)
-print im.shape
-plt.imshow(im,cmap='gray')
-plt.show()
-
+from ImageConvolution import *
+imageConvolution = ImageConvolution('wenger.png')
 ga = np.zeros((20,20))
 for i in range(20):
     for j in range(20):
         dist = (i-9.5)**2 + (j-9.5)**2
         ga[i][j] = np.exp(-dist/50)
-plt.imshow(ga,cmap='gray')
-plt.show()
-im_ga_c = convolve2d(im,ga)
-plt.imshow(im_ga_c,cmap='gray')
-plt.show()
+imageConvolution.convolve_and_plot(ga)
